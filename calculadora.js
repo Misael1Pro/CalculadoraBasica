723080525891d81496f692fda9b2d7c5a66b640f
Misael <betancourthmisael2001@gmail.com>
2023-09-16 23:08:09 -0600

    Calculadora Basica

---
+++ calculadora.js
@@ -0,0 +1,25 @@
+let numero1=20
+let numero2=10
+
+function Sumarnumeros(){
+suma = numero1+numero2;
+console.log("La suma de "+numero1+ " mas "+numero2+" es "+suma)
+}
+function restarnumeros(){
+resta= numero1-numero2
+console.log("La resta de "+numero1+ " menos "+numero2+" es "+resta)
+}
+function multiplicarnumeros(){
+multiplicar=numero1*numero2
+console.log("La Multiplicacion de "+numero1+ " por "+numero2+" es "+multiplicar)
+
+}
+function divinumeros(){
+dividir=numero1/numero2
+console.log("La Division de "+numero1+ " entre "+numero2+" es "+dividir)
+}
+
+Sumarnumeros()
+restarnumeros()
+multiplicarnumeros()
+divinumeros()
